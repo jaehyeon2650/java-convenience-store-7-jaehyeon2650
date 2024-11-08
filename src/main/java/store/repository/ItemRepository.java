5c2ccd54541c2c8f02fe9813d9a18981aa707f60
wogus <ind07152@naver.com>
2024-11-08 20:49:28 +0900

    feat(ItemRepository): 주문 예외 처리 기능 추가
    창고에 없는 상품을 주문하거나 재고 수량보다 많은 주문을 하면 예외가 발생한다.

--- src/main/java/store/repository/ItemRepository.java
+++ src/main/java/store/repository/ItemRepository.java
@@ -4,9 +4,29 @@ import java.util.List;
 import store.domain.Item;
 
 public class ItemRepository {
-    private List<Item> items;
+    private final List<Item> items;
 
     public ItemRepository(List<Item> items) {
         this.items = items;
+    }
+
+    public void validatePurchase(String itemName, int count) {
+        validateItemName(itemName);
+        validateItemCount(itemName, count);
+    }
+
+    private void validateItemName(String itemName) {
+        boolean hasItemName = items.stream().anyMatch(item -> item.getName().equals(itemName));
+        if (!hasItemName) {
+            throw new IllegalArgumentException("[ERROR] 존재하지 않는 상품입니다. 다시 입력해 주세요.");
+        }
+    }
+
+    private void validateItemCount(String itemName, int count) {
+        int sum = items.stream().filter(item -> item.getName().equals(itemName))
+                .mapToInt(Item::getQuantity).sum();
+        if (sum < count) {
+            throw new IllegalArgumentException("[ERROR] 재고 수량을 초과하여 구매할 수 없습니다. 다시 입력해 주세요.");
+        }
     }
 }
