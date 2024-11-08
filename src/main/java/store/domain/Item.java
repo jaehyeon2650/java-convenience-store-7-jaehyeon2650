57146b9bc7d6e151f27b4c03dd1d6bdd35d7575d
wogus <ind07152@naver.com>
2024-11-08 18:22:41 +0900

    feat(Item): item 도메인 추가
    편의점 아이템 정보를 담는 item 도메인을 추가한다.

---
+++ src/main/java/store/domain/Item.java
@@ -0,0 +1,15 @@
+package store.domain;
+
+public class Item {
+    private String name;
+    private int price;
+    private int quantity;
+    private Promotion promotion;
+
+    public Item(String name, int price, int quantity, Promotion promotion) {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+        this.promotion = promotion;
+    }
+}
