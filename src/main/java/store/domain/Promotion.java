55b2a4d7cf3e6029cab8f9e740db7dd5914db102
wogus <ind07152@naver.com>
2024-11-09 22:53:23 +0900

    feat(Promotion): 프로모션 결과 도출 기능 추가
    창고의 있는 개수와 사용자가 원하는 개수, 이벤트의 종류를 토대로 프로모션 적용 결과를 PromotionResponseDto에 담아 반환한다.

--- src/main/java/store/domain/Promotion.java
+++ src/main/java/store/domain/Promotion.java
@@ -1,7 +1,12 @@
 package store.domain;
 
-import java.time.LocalDate;
+import static store.domain.PromotionResult.APPLY_REGULAR_PRICE;
+import static store.domain.PromotionResult.REQUIRE_ADDITIONAL_ITEM;
+import static store.domain.PromotionResult.SUCCESS;
+
+import java.time.LocalDateTime;
 import java.util.Objects;
+import store.dto.response.PromotionResponseDto;
 
 public class Promotion {
     private String name;
@@ -42,4 +47,30 @@ public class Promotion {
     public boolean canApplyPromotion(LocalDateTime time) {
         return startDate.isBefore(time) && endDate.isAfter(time);
     }
+
+    public PromotionResponseDto getPromotionResult(int stockQuantity, int purchase) {
+        if (stockQuantity > purchase && purchase % (buy + get) == buy) {
+            int set = purchase / (buy + get);
+            int buyCount = set * buy + buy;
+            int getCount = set * get;
+            int extraBuy = 0;
+            int extraGet = 1;
+            return PromotionResponseDto.of(REQUIRE_ADDITIONAL_ITEM, buyCount, getCount, extraBuy, extraGet);
+        } else if (stockQuantity < purchase || (stockQuantity == purchase && purchase % (get + buy) == buy)) {
+            int set = stockQuantity / (buy + get);
+            int buyCount = set * buy;
+            int getCount = set * get;
+            int extraGet = 0;
+            int extraBuy = (purchase - stockQuantity) + (stockQuantity) % (buy + get);
+            return PromotionResponseDto.of(APPLY_REGULAR_PRICE, buyCount, getCount, extraBuy, extraGet);
+        } else {
+            int set = purchase / (buy + get);
+            int buyCount = set * buy + (purchase) % (buy + get);
+            int getCount = set * get;
+            int extraBuy = 0;
+            int extraGet = 0;
+            return PromotionResponseDto.of(SUCCESS, buyCount, getCount, extraBuy, extraGet);
+        }
+    }
+
 }
