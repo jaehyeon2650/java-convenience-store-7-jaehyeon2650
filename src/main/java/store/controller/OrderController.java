003bf5c1e0831538621eb7a9ad97918e73c4aae0
wogus <ind07152@naver.com>
2024-11-09 23:16:37 +0900

    feat(OrderController): 최종 결제 요청을 생성하는 로직 추가
    프로모션 적용 결과를 바탕으로 최종 결제 요청을 로직을 추가한다.

--- src/main/java/store/controller/OrderController.java
+++ src/main/java/store/controller/OrderController.java
@@ -4,6 +4,7 @@ import camp.nextstep.edu.missionutils.DateTimes;
 import java.util.ArrayList;
 import java.util.List;
 import store.domain.PromotionResult;
+import store.dto.request.PaymentRequestDto;
 import store.dto.response.PromotionInfoResponseDto;
 import store.dto.response.PromotionResponseDto;
 import store.service.OrderService;
@@ -33,6 +34,51 @@ public class OrderController {
                             DateTimes.now()));
                 }
                 return infoResponseDtos;
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+    private List<PaymentRequestDto> makePaymentsRequest(List<PromotionInfoResponseDto> promotionInfoResponseDtos) {
+        List<PaymentRequestDto> paymentRequestDtos = new ArrayList<>();
+        for (PromotionInfoResponseDto promotionInfoResponseDto : promotionInfoResponseDtos) {
+            paymentRequestDtos.add(makePaymentRequest(promotionInfoResponseDto));
+        }
+        return paymentRequestDtos;
+    }
+
+    private PaymentRequestDto makePaymentRequest(PromotionInfoResponseDto promotionInfoResponseDto) {
+        PromotionResponseDto promotion = promotionInfoResponseDto.promotionResponseDto();
+        String name = promotionInfoResponseDto.name();
+        if (promotionInfoResponseDto.getPromotionResult() == PromotionResult.APPLY_REGULAR_PRICE) {
+            if (checkExtraPayment(name, promotion.extraBuy())) {
+                return PaymentRequestDto.of(name, promotion.buyCount() + promotion.extraBuy(), promotion.getCount());
+            }
+            return PaymentRequestDto.of(name, promotion.buyCount(), promotion.getCount());
+        }
+        if (promotionInfoResponseDto.getPromotionResult() == PromotionResult.REQUIRE_ADDITIONAL_ITEM) {
+            if (checkAddFreeItem(name)) {
+                return PaymentRequestDto.of(name, promotion.buyCount(), promotion.getCount() + promotion.extraGet());
+            }
+        }
+        return PaymentRequestDto.of(name, promotion.buyCount(), promotion.getCount());
+    }
+
+    private boolean checkAddFreeItem(String name) {
+        while (true) {
+            try {
+                return inputView.chooseAddFreeItem(name);
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+    private boolean checkExtraPayment(String name, int count) {
+        while (true) {
+            try {
+                return inputView.chooseExtraPayment(name, count);
             } catch (IllegalArgumentException e) {
                 System.out.println(e.getMessage());
             }
