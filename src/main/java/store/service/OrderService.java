76b5eaa77118b1dbe89a1e110c57480fb48dec85
wogus <ind07152@naver.com>
2024-11-09 23:02:56 +0900

    feat(OrderService): 품목에 대해 프로모션을 적용하고 그 결과를 반환하는 로직 추가
    사용자가 입력한 품목 중 하나에 대해 프로모션을 적용하고 그 결과를 반환하는 로직을 추가한다.

--- src/main/java/store/service/OrderService.java
+++ src/main/java/store/service/OrderService.java
@@ -1,8 +1,14 @@
 package store.service;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import store.domain.Item;
 import store.domain.Items;
 import store.dto.response.ItemResponseDto;
 import store.dto.response.ItemsResponseDto;
+import store.dto.response.PromotionInfoResponseDto;
+import store.dto.response.PromotionResponseDto;
 
 public class OrderService {
 
@@ -11,10 +17,17 @@ public class OrderService {
     public OrderService(Items items) {
         this.items = items;
     }
+
     public ItemsResponseDto getItemList() {
         List<Item> itemList = items.getItems();
         List<ItemResponseDto> list = itemList.stream().map(ItemResponseDto::of).toList();
         return ItemsResponseDto.from(list);
+    }
+
+    public PromotionInfoResponseDto getOrderInfo(String name, int count, LocalDateTime orderDate) {
+        items.validatePurchase(name, count);
+        PromotionResponseDto promotionResult = items.getPromotionResult(name, count, orderDate);
+        return PromotionInfoResponseDto.of(name, promotionResult);
     }
 
 }
