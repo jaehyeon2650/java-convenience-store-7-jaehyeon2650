12a4777f4e0a17ce499b5e069110122f52201137
wogus <ind07152@naver.com>
2024-11-09 23:11:39 +0900

    feat(OrderController): 주문 입력과 프로모션 적용 기능 추가
    사용자로부터 주문 입력을 받고 프로모션 적용 결과를 반환하는 기능을 추가한다.

--- src/main/java/store/controller/OrderController.java
+++ src/main/java/store/controller/OrderController.java
@@ -1,5 +1,11 @@
 package store.controller;
 
+import camp.nextstep.edu.missionutils.DateTimes;
+import java.util.ArrayList;
+import java.util.List;
+import store.domain.PromotionResult;
+import store.dto.response.PromotionInfoResponseDto;
+import store.dto.response.PromotionResponseDto;
 import store.service.OrderService;
 import store.view.InputView;
 import store.view.OutputView;
@@ -14,4 +20,23 @@ public class OrderController {
         this.inputView = inputView;
         this.orderService = orderService;
     }
+
+    private List<PromotionInfoResponseDto> makeOrderInfo() {
+        while (true) {
+            try {
+                List<PromotionInfoResponseDto> infoResponseDtos = new ArrayList<>();
+                OrdersRequestDto orders = inputView.getOrders();
+                List<OrderRequestDto> orderRequestDtos = orders.orderList();
+                for (OrderRequestDto orderRequestDto : orderRequestDtos) {
+                    infoResponseDtos.add(orderService.getOrderInfo(orderRequestDto.name(),
+                            orderRequestDto.quantity(),
+                            DateTimes.now()));
+                }
+                return infoResponseDtos;
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
 }
