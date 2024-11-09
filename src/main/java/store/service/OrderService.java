788bb09639e90e6593e9b8da39a6652e759cd4c9
wogus <ind07152@naver.com>
2024-11-09 23:25:51 +0900

    feat(OrderService): 최종 결제 요청을 바탕으로 결제를 진행 후 영수증 반환하는 기능 추가
    최종 결제 요청을 바탕으로 재고를 줄이고 맴버십 할인에 따라 결제 결과 영수증을 반환한다.

--- src/main/java/store/service/OrderService.java
+++ src/main/java/store/service/OrderService.java
@@ -5,10 +5,13 @@ import java.util.ArrayList;
 import java.util.List;
 import store.domain.Item;
 import store.domain.Items;
+import store.dto.request.PaymentRequestDto;
 import store.dto.response.ItemResponseDto;
 import store.dto.response.ItemsResponseDto;
 import store.dto.response.PromotionInfoResponseDto;
 import store.dto.response.PromotionResponseDto;
+import store.dto.response.ReceiptResponseDto;
+import store.dto.response.ReceiptsResponseDto;
 
 public class OrderService {
 
@@ -30,4 +33,41 @@ public class OrderService {
         return PromotionInfoResponseDto.of(name, promotionResult);
     }
 
+    public ReceiptsResponseDto payment(List<PaymentRequestDto> paymentRequestDtos, boolean memberShip) {
+        List<ReceiptResponseDto> receipts = new ArrayList<>();
+        for (PaymentRequestDto paymentRequestDto : paymentRequestDtos) {
+            String itemName = paymentRequestDto.name();
+            int count = paymentRequestDto.buy() + paymentRequestDto.get();
+            int price = items.getItemPrice(itemName);
+            items.purchaseItem(itemName, count);
+            int benefit = price * paymentRequestDto.get();
+            receipts.add(new ReceiptResponseDto(itemName, count, count * price, paymentRequestDto.get(), benefit));
+        }
+        return makeReceiptsResponseDto(receipts, memberShip);
+    }
+
+    private ReceiptsResponseDto makeReceiptsResponseDto(List<ReceiptResponseDto> receipts, boolean memberShip) {
+        int totalPrice = 0;
+        int totalCount = 0;
+        int promotionDiscount = 0;
+        int notBenefitPrice = 0;
+        for (ReceiptResponseDto receipt : receipts) {
+            totalPrice += receipt.itemsPrice();
+            totalCount += receipt.totalCount();
+            promotionDiscount += receipt.benefit();
+            if (receipt.benefit() == 0) {
+                notBenefitPrice += receipt.itemsPrice();
+            }
+        }
+        int membershipBenefit = (int) (notBenefitPrice * 0.3);
+        if (membershipBenefit > 8000) {
+            membershipBenefit = 8000;
+        }
+        if (memberShip) {
+            return new ReceiptsResponseDto(totalPrice, totalCount, promotionDiscount, membershipBenefit,
+                    totalPrice - promotionDiscount - membershipBenefit, receipts);
+        }
+        return new ReceiptsResponseDto(totalPrice, totalCount, promotionDiscount, 0, totalPrice - promotionDiscount,
+                receipts);
+    }
 }
