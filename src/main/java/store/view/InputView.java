7e48cefbb7eaa2e8c5a52cd0ef994e6251548b34
wogus <ind07152@naver.com>
2024-11-09 22:30:48 +0900

    feat(InputView): 주문 입력 관련 검증 추가
    올바른 형식으로 입력되었는지 검증한다.

--- src/main/java/store/view/InputView.java
+++ src/main/java/store/view/InputView.java
@@ -1,6 +1,8 @@
 package store.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import store.dto.request.OrdersRequestDto;
 import store.util.Parser;
 
@@ -10,6 +12,19 @@ public class InputView {
         String orderList = Console.readLine().trim();
         System.out.println();
         String[] words = orderList.split(",");
+        Validator.validateInputForm(words);
         return Parser.parseToOrders(words);
+    }
+    private static class Validator {
+        private static final Pattern ITEM_PATTERN = Pattern.compile("^\\[[^\\[]+-\\d+\\]$");
+
+        public static void validateInputForm(String[] words) {
+            for (String word : words) {
+                Matcher matcher = ITEM_PATTERN.matcher(word.trim());
+                if (!matcher.matches()) {
+                    throw new IllegalArgumentException("[ERROR] 올바르지 않은 형식으로 입력했습니다. 다시 입력해 주세요.");
+                }
+            }
+        }
     }
 }
