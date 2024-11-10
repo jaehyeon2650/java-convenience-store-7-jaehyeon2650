33916b1ca38a5a080c364b79d3295323874dd5d0
wogus <ind07152@naver.com>
2024-11-10 19:39:05 +0900

    refactor(StoreException): 커스텀 예외로 변경
    IllegalArgumentException을 커스텀 예외로 반환한다.

--- src/main/java/store/util/Parser.java
+++ src/main/java/store/util/Parser.java
@@ -1,10 +1,14 @@
 package store.util;
 
+import static store.exception.ErrorMessage.INVALID_INPUT;
+import static store.exception.ErrorMessage.INVALID_INPUT_FORMAT;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import store.dto.request.OrderRequestDto;
 import store.dto.request.OrdersRequestDto;
+import store.exception.StoreException;
 
 public class Parser {
     public static OrdersRequestDto parseToOrders(String[] words) {
@@ -12,7 +16,6 @@ public class Parser {
         Arrays.stream(words).forEach(
                 word -> {
                     word = word.substring(1, word.length() - 1);
-                    System.out.println(word);
                     String[] order = word.trim().split("-");
                     Validator.validateSplitResultSize(order);
                     orderList.add(new OrderRequestDto(order[0].trim(), Validator.validateNumber(order[1].trim())));
@@ -24,7 +27,7 @@ public class Parser {
     private static class Validator {
         public static int validateNumber(String number) {
             if (!number.matches("\\d+")) {
-                throw new IllegalArgumentException("[ERROR] 잘못된 입력입니다. 다시 입력해 주세요.");
+                throw StoreException.from(INVALID_INPUT);
             }
             return changeTONumber(number);
         }
@@ -32,14 +35,14 @@ public class Parser {
         private static int changeTONumber(String number) {
             int result = Integer.parseInt(number);
             if (result <= 0) {
-                throw new IllegalArgumentException("[ERROR] 잘못된 입력입니다. 다시 입력해 주세요.");
+                throw StoreException.from(INVALID_INPUT);
             }
             return result;
         }
 
         public static void validateSplitResultSize(String[] order) {
             if (order.length != 2) {
-                throw new IllegalArgumentException("[ERROR] 올바르지 않은 형식으로 입력했습니다. 다시 입력해 주세요.");
+                throw StoreException.from(INVALID_INPUT_FORMAT);
             }
         }
     }
