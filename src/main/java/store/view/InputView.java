bdba2d453abf862ce5d6f56bc94950a9e31bec49
wogus <ind07152@naver.com>
2024-11-10 21:24:21 +0900

    refactor(InputView): 출력문 변경

--- src/main/java/store/view/InputView.java
+++ src/main/java/store/view/InputView.java
@@ -21,12 +21,12 @@ public class InputView {
     }
 
     public boolean chooseAddFreeItem(String name, int count) {
-        System.out.println("현재 " + name + "은(는) " + count + "개를 무료로 더 받을 수 있습니다. 추가하시겠습니까? (Y/N)");
+        System.out.printf("현재 %s은(는) %d개를 무료로 더 받을 수 있습니다. 추가하시겠습니까? (Y/N)\n", name, count);
         return getAnswer();
     }
 
     public boolean chooseExtraPayment(String name, int count) {
-        System.out.println("현재 " + name + " " + count + "개는 프로모션 할인이 적용되지 않습니다. 그래도 구매하시겠습니까? (Y/N)");
+        System.out.printf("현재 %s %d개는 프로모션 할인이 적용되지 않습니다. 그래도 구매하시겠습니까? (Y/N)\n", name, count);
         return getAnswer();
     }
 
