81688a9fb832e44001daadb3b4557bb7f74b30c6
wogus <ind07152@naver.com>
2024-11-10 19:33:45 +0900

    fix(StoreException): 예외 상속 수정
    IllegalArgumentException을 상속 받는다.

--- src/main/java/store/exception/StoreException.java
+++ src/main/java/store/exception/StoreException.java
@@ -1,6 +1,6 @@
 package store.exception;
 
-public class StoreException extends RuntimeException {
+public class StoreException extends IllegalArgumentException {
     public static final String PREFIX = "[ERROR] ";
 
     private StoreException(ErrorMessage errorMessage) {
