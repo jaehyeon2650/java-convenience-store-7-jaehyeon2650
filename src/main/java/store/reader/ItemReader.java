48cf451ecaa49437078ab8b92e9d24e9d7a9393e
wogus <ind07152@naver.com>
2024-11-10 21:10:17 +0900

    refactor(ItemReader): 메서드 분리

--- src/main/java/store/reader/ItemReader.java
+++ src/main/java/store/reader/ItemReader.java
@@ -1,34 +1,32 @@
 package store.reader;
 
-import java.io.BufferedReader;
-import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import store.domain.Item;
 import store.domain.Promotion;
+import store.util.Reader;
 
 public class ItemReader {
-
     public List<Item> readItems(String file, List<Promotion> promotions) throws IOException {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(file)));
+        List<String> lines = Reader.readFile(file);
         List<Item> items = new ArrayList<>();
-        reader.readLine();
-        String line;
-        while ((line = reader.readLine()) != null) {
-            String[] parts = line.split(",");
-            String name = parts[0];
-            int price = Integer.parseInt(parts[1]);
-            int quantity = Integer.parseInt(parts[2]);
-            String promotionsName = parts[3];
-
-            Optional<Promotion> promotionOptional = promotions.stream()
-                    .filter(promotion -> promotion.getPromotionName().equals(promotionsName)).findAny();
-            items.add(new Item(name, price, quantity, promotionOptional.orElse(null)));
-        }
+        lines.forEach(line -> {
+            items.add(parseItem(line, promotions));
+        });
         return addRegularStock(items);
+    }
+
+    private Item parseItem(String line, List<Promotion> promotions) {
+        String[] parts = line.split(",");
+        String name = parts[0];
+        int price = Integer.parseInt(parts[1]);
+        int quantity = Integer.parseInt(parts[2]);
+        String promotionsName = parts[3];
+        Optional<Promotion> promotionOptional = promotions.stream()
+                .filter(promotion -> promotion.getPromotionName().equals(promotionsName)).findAny();
+        return new Item(name, price, quantity, promotionOptional.orElse(null));
     }
 
     private List<Item> addRegularStock(List<Item> items) {
