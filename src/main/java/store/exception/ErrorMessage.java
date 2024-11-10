e93181057d364ea6e0e7a6d2904619fe71d9b7ee
wogus <ind07152@naver.com>
2024-11-10 22:57:04 +0900

    feat(ErrorMessage): 예외 메시지 추가
    파일 입출력 관련 예외 메시지를 추가한다.

--- src/main/java/store/exception/ErrorMessage.java
+++ src/main/java/store/exception/ErrorMessage.java
@@ -4,7 +4,8 @@ public enum ErrorMessage {
     INVALID_INPUT_FORMAT("올바르지 않은 형식으로 입력했습니다. 다시 입력해 주세요."),
     INVALID_ITEM("존재하지 않는 상품입니다. 다시 입력해 주세요."),
     INVALID_QUANTITY("재고 수량을 초과하여 구매할 수 없습니다. 다시 입력해 주세요."),
-    INVALID_INPUT("잘못된 입력입니다. 다시 입력해 주세요.");
+    INVALID_INPUT("잘못된 입력입니다. 다시 입력해 주세요."),
+    FILE_PROBLEM("파일을 읽는 중에 실패하였습니다. 파일명을 확인해주세요.");
     private String message;
 
     ErrorMessage(String message) {
