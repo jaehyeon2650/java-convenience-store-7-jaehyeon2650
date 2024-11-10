132aa493cedd1da29875e76b49059e28cdab4314
wogus <ind07152@naver.com>
2024-11-10 19:29:34 +0900

    feat(Parser): 숫자 예외 검증 추가
    주문한 수량은 1개 이상이여야한다는 검증을 추가한다.

--- src/main/java/store/util/Parser.java
+++ src/main/java/store/util/Parser.java
@@ -12,20 +12,29 @@ public class Parser {
         Arrays.stream(words).forEach(
                 word -> {
                     word = word.substring(1, word.length() - 1);
+                    System.out.println(word);
                     String[] order = word.trim().split("-");
                     Validator.validateSplitResultSize(order);
-                    Validator.validateNumber(order[1]);
-                    orderList.add(new OrderRequestDto(order[0], Integer.parseInt(order[1])));
+                    orderList.add(new OrderRequestDto(order[0].trim(), Validator.validateNumber(order[1].trim())));
                 }
         );
         return new OrdersRequestDto(orderList);
     }
 
     private static class Validator {
-        public static void validateNumber(String number) {
+        public static int validateNumber(String number) {
             if (!number.matches("\\d+")) {
-                throw new IllegalArgumentException("[ERROR] 올바르지 않은 형식으로 입력했습니다. 다시 입력해 주세요.");
+                throw new IllegalArgumentException("[ERROR] 잘못된 입력입니다. 다시 입력해 주세요.");
             }
+            return changeTONumber(number);
+        }
+
+        private static int changeTONumber(String number) {
+            int result = Integer.parseInt(number);
+            if (result <= 0) {
+                throw new IllegalArgumentException("[ERROR] 잘못된 입력입니다. 다시 입력해 주세요.");
+            }
+            return result;
         }
 
         public static void validateSplitResultSize(String[] order) {
