b11a81c4b30344f783d01d200b81a9dfa47676f2
wogus <ind07152@naver.com>
2024-11-10 21:10:23 +0900

    refactor(PromotionReader): 메서드 분리

--- src/main/java/store/reader/PromotionReader.java
+++ src/main/java/store/reader/PromotionReader.java
@@ -1,31 +1,30 @@
 package store.reader;
 
-import java.io.BufferedReader;
-import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 import store.domain.Promotion;
+import store.util.Reader;
 
 public class PromotionReader {
+
     public List<Promotion> readPromotions(String file) throws IOException {
+        List<String> lines = Reader.readFile(file);
         List<Promotion> promotions = new ArrayList<>();
-        BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(file)));
-
-        br.readLine();
-        String line;
-        while ((line = br.readLine()) != null) {
-            String[] parts = line.split(",");
-            String name = parts[0];
-            int buy = Integer.parseInt(parts[1]);
-            int get = Integer.parseInt(parts[2]);
-            LocalDate startDate = LocalDate.parse(parts[3]);
-            LocalDate endDate = LocalDate.parse(parts[4]);
-            promotions.add(new Promotion(name, buy, get, startDate.atStartOfDay(), endDate.atStartOfDay()));
-        }
-
+        lines.forEach(line -> {
+            promotions.add(parsePromotion(line));
+        });
         return promotions;
+    }
+
+    private Promotion parsePromotion(String line) {
+        String[] parts = line.split(",");
+        String name = parts[0];
+        int buy = Integer.parseInt(parts[1]);
+        int get = Integer.parseInt(parts[2]);
+        LocalDate startDate = LocalDate.parse(parts[3]);
+        LocalDate endDate = LocalDate.parse(parts[4]);
+        return new Promotion(name, buy, get, startDate.atStartOfDay(), endDate.atStartOfDay());
     }
 }
