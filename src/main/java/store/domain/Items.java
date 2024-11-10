a7adb9741bb0cc67fd29c885c3122d414779328b
wogus <ind07152@naver.com>
2024-11-10 17:22:24 +0900

    refactor(Items): 내부 검증 클래스 추가
    내부 검증 클래스를 만들어서 검증을 한다.

--- src/main/java/store/domain/Items.java
+++ src/main/java/store/domain/Items.java
@@ -46,22 +46,25 @@ public class Items {
     }
 
     public void validatePurchase(String itemName, int count) {
-        validateItemName(itemName);
-        validateItemCount(itemName, count);
+        Validator.validateItemName(items, itemName);
+        Validator.validateItemCount(items, itemName, count);
     }
 
-    private void validateItemName(String itemName) {
-        boolean hasItemName = items.stream().anyMatch(item -> item.getName().equals(itemName));
-        if (!hasItemName) {
-            throw new IllegalArgumentException("[ERROR] 존재하지 않는 상품입니다. 다시 입력해 주세요.");
+    private static class Validator {
+        public static void validateItemName(List<Item> items, String itemName) {
+            boolean hasItemName = items.stream().anyMatch(item -> item.getName().equals(itemName));
+            if (!hasItemName) {
+                throw new IllegalArgumentException("[ERROR] 존재하지 않는 상품입니다. 다시 입력해 주세요.");
+            }
+        }
+
+        public static void validateItemCount(List<Item> items, String itemName, int count) {
+            int sum = items.stream().filter(item -> item.getName().equals(itemName))
+                    .mapToInt(Item::getQuantity).sum();
+            if (sum < count) {
+                throw new IllegalArgumentException("[ERROR] 재고 수량을 초과하여 구매할 수 없습니다. 다시 입력해 주세요.");
+            }
         }
     }
 
-    private void validateItemCount(String itemName, int count) {
-        int sum = items.stream().filter(item -> item.getName().equals(itemName))
-                .mapToInt(Item::getQuantity).sum();
-        if (sum < count) {
-            throw new IllegalArgumentException("[ERROR] 재고 수량을 초과하여 구매할 수 없습니다. 다시 입력해 주세요.");
-        }
-    }
 }
