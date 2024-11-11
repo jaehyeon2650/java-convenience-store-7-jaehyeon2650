14d4ae0a54dbfe86ab2603df3738686b65aa2582
wogus <ind07152@naver.com>
2024-11-11 16:02:20 +0900

    refactor(Reader): 메소드 최적화

--- src/main/java/store/util/Reader.java
+++ src/main/java/store/util/Reader.java
@@ -4,19 +4,12 @@ import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.List;
 
 public class Reader {
     public static List<String> readFile(String file) throws IOException {
         try (BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(file)))) {
-            List<String> lines = new ArrayList<>();
-            reader.readLine();
-            String line;
-            while ((line = reader.readLine()) != null) {
-                lines.add(line);
-            }
-            return lines;
+            return reader.lines().skip(1).toList();
         }
     }
 }
