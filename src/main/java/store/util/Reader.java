691e95bbbc0c2e4ef493264d9c4542915a050d59
wogus <ind07152@naver.com>
2024-11-15 11:57:08 +0900

    refactor(Reader): 상수화

--- src/main/java/store/util/Reader.java
+++ src/main/java/store/util/Reader.java
@@ -7,9 +7,11 @@ import java.io.InputStreamReader;
 import java.util.List;
 
 public class Reader {
+    private static final int HEADER_LINE = 1;
+
     public static List<String> readFile(String file) throws IOException {
         try (BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(file)))) {
-            return reader.lines().skip(1).toList();
+            return reader.lines().skip(HEADER_LINE).toList();
         }
     }
 }
