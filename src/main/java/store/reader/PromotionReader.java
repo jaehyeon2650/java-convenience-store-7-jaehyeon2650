f6209e443e6f7214b6219f366a4e205cceab8c1b
wogus <ind07152@naver.com>
2024-11-15 11:57:26 +0900

    refactor(PromotionReader): 상수화

--- src/main/java/store/reader/PromotionReader.java
+++ src/main/java/store/reader/PromotionReader.java
@@ -8,6 +8,12 @@ import store.domain.Promotion;
 import store.util.Reader;
 
 public class PromotionReader {
+    private static final String DELIMITER = ",";
+    private static final int NAME_INDEX = 0;
+    private static final int BUY_COUNT_INDEX = 1;
+    private static final int GET_COUNT_INDEX = 2;
+    private static final int START_DATE_INDEX = 3;
+    private static final int END_DATE_INDEX = 4;
 
     public List<Promotion> readPromotions(String file) throws IOException {
         List<String> lines = Reader.readFile(file);
@@ -19,12 +25,12 @@ public class PromotionReader {
     }
 
     private Promotion parsePromotion(String line) {
-        String[] parts = line.split(",");
-        String name = parts[0];
-        int buy = Integer.parseInt(parts[1]);
-        int get = Integer.parseInt(parts[2]);
-        LocalDate startDate = LocalDate.parse(parts[3]);
-        LocalDate endDate = LocalDate.parse(parts[4]);
+        String[] parts = line.split(DELIMITER);
+        String name = parts[NAME_INDEX];
+        int buy = Integer.parseInt(parts[BUY_COUNT_INDEX]);
+        int get = Integer.parseInt(parts[GET_COUNT_INDEX]);
+        LocalDate startDate = LocalDate.parse(parts[START_DATE_INDEX]);
+        LocalDate endDate = LocalDate.parse(parts[END_DATE_INDEX]);
         return new Promotion(name, buy, get, startDate.atStartOfDay(), endDate.atStartOfDay());
     }
 }
